7bc77afcfbb2f064b5c2b86ec9bee0049ae64398
t4iler <s4tybaev02@gmail.com>
2022-12-18 19:54:12 +0600

    settings

--- web_tech/settings.py
+++ web_tech/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', cast=str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.102.1.182']
 
 
 # Application definition
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'web_tech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
